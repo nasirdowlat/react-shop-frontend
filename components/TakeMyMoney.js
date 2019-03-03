bc4d8aee83deaf996e234676d19155a4b994cac2
Nasir <nasir@scalestack.io>
2019-03-03 18:02:08 -0500

    complete user checkout

--- components/TakeMyMoney.js
+++ components/TakeMyMoney.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 import { Mutation } from 'react-apollo';
@@ -10,37 +12,60 @@ import User, { CURRENT_USER_QUERY } from './User';
 import calcTotalPrice from '../lib/calcTotalPrice';
 import { stripeKey } from '../config';
 
+const CREATE_ORDER_MUTATION = gql`
+  mutation createOrder($token: String!) {
+    createOrder(token: $token) {
+      id
+      charge
+      total
+      items {
+        id
+        title
+      }
+    }
+  }
+`;
+
 function totalItems(cart) {
   return cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0);
 }
 
 class TakeMyMoney extends React.Component {
-  onToken = res => {
+  onToken = async (res, createOrder) => {
     const tokenId = res.id;
+    // manually call the mutation
+    const order = await createOrder({
+      variables: {
+        token: res.id,
+      },
+    }).catch(e => alert(e.message));
+    console.log(order);
   };
 
   render() {
     return (
       <User>
-        {({ data: { me } }) => {
-          if (me.cart === null || me.cart.length === 0) {
-            return null;
-          }
-          return (
-            <StripeCheckout
-              amount={calcTotalPrice(me.cart)}
-              name="React Shop"
-              description={`Order of ${totalItems(me.cart)}`}
-              image={me.cart[0].item && me.cart[0].item.image}
-              stripeKey={stripeKey}
-              currency="CAD"
-              email={me.email}
-              token={res => this.onToken(res)}
-            >
-              {this.props.children}
-            </StripeCheckout>
-          );
-        }}
+        {({ data: { me } }) => (
+          <Mutation
+            mutation={CREATE_ORDER_MUTATION}
+            refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+          >
+            {createOrder => (
+              <StripeCheckout
+                amount={me.cart.length && calcTotalPrice(me.cart)}
+                name="React Shop"
+                description={`Order of ${totalItems(me.cart)}`}
+                image={me.cart.lenth && me.cart[0].item.image}
+                stripeKey={stripeKey}
+                currency="CAD"
+                email={me.email}
+                token={res => this.onToken(res, createOrder)}
+              >
+                {this.props.children}
+              </StripeCheckout>
+            )}
+          </Mutation>
+        )}
       </User>
     );
   }
